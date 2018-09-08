12b29809272060455762aa35a17c209d73cb9780
KrylovAS <s1nii@mail.ru>
2018-09-08 13:19:41 +0400

    done

--- jsx/store/js/ShopItem.js
+++ jsx/store/js/ShopItem.js
@@ -1,12 +1,11 @@
 'use strict';
 
 
-const ShopItem = function(items) {
+const ShopItem =({item}) => {    
+    const {brand, title, description, descriptionFull, price, currency} = item;
     
-    const {brand, title, description, descriptionFull, price, currency} = items.item;
-    console.log(item)
     return (
-    <div className="main-content">
+      <div className="main-content">
         <h2>{brand}</h2> 
         <h1>{title}</h1>
         <h3>{description}</h3>
@@ -14,10 +13,10 @@ const ShopItem = function(items) {
         <div className="highlight-window  mobile"><div className="highlight-overlay"></div></div>
         <div className="divider"></div>
         <div className="purchase-info">
-            <div className="price">{currency}{price.toFixed(2)}</div>
-            <button>Добавить в корзину</button>
-         </div>
-    </div>
+          <div className="price">{currency}{price.toFixed(2)}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
     );
 }
 

a6c0ab34c5ac8d0ec5c352a21c9b5642a5f28c2b
KrylovAS <s1nii@mail.ru>
2018-09-11 11:24:27 +0400

    refactoring

--- jsx/menu/js/Menu.js
+++ jsx/menu/js/Menu.js
@@ -1,24 +1,20 @@
 'use strict';
 
-const createList = items => {
-  return (
-    <ul>
-      {items.map(el => (
-        <li>
-          <a href={el.href}>{el.title}</a>
-        </li>
-      ))}
-    </ul>
-  );
-};
-
 const Menu = menu => {
   return menu.opened ? (
     <div className="menu menu-open">
       <div className="menu-toggle">
         <span />
       </div>
-      <nav>{createList(menu.items)}</nav>
+      <nav>
+        <ul>
+          {items.map(el => (
+            <li>
+              <a href={el.href}>{el.title}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   ) : (
     <div className="menu">

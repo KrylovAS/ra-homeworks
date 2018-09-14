6c970934a89e7ddc4c56d16e6e239c7458541f85
KrylovAS <s1nii@mail.ru>
2018-09-15 03:27:05 +0400

    create MessageHistory component

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function MessageHistory({list=[]}) {
+  const messageBlock =  list.map(message => { 
+    const {id, from, type, ...data} = message;
+    
+    switch(type){
+      case 'response': return <Response key={id} message={data} from={from}/>;      
+      case 'message': return <Message key={id} message={data} from={from}/>;          
+      default: return <Typing key={id} message={data} from={from}/>
+    } 
+  });
+
+  return (
+    <ul>  
+      {messageBlock}
+    </ul>
+  );
+}

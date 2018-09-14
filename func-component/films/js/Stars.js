e1da19335b5854fce750fa0974121c723c8bcedb
KrylovAS <s1nii@mail.ru>
2018-09-14 17:53:02 +0400

    create Stars component

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,21 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars({count}) {
+  const createStar = function(count){
+    
+    const  quantityStar= [];  
+    if( typeof count === "number" && count >= 1 && count <= 5 ) {        
+      for (let i = 0; i < count; i++) {
+        quantityStar.push(<Star/>);
+      }
+      return quantityStar.map(el => <li>{el}</li>);
+    }
+
+    return null;
+}
+  return (
+   <ul className="card-body-stars u-clearfix">
+    {createStar(count)}
+  </ul>
+  );
 }

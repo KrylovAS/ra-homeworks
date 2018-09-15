34aa0f373317fc084f5546bfe13509335e63b4b2
KrylovAS <s1nii@mail.ru>
2018-09-15 10:56:04 +0400

    refactoring

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,19 +1,18 @@
 'use strict';
 
-function MessageHistory({list=[]}) {
-  const messageBlock =  list.map(message => { 
-    const {id, from, type, ...data} = message;
-    
-    switch(type){
-      case 'response': return <Response key={id} message={data} from={from}/>;      
-      case 'message': return <Message key={id} message={data} from={from}/>;          
-      default: return <Typing key={id} message={data} from={from}/>
-    } 
+function MessageHistory({ list = [] }) {
+  const messageBlock = list.map(message => {
+    const { id, from, type, ...data } = message;
+
+    switch (type) {
+      case 'response':
+        return <Response key={id} message={data} from={from} />;
+      case 'message':
+        return <Message key={id} message={data} from={from} />;
+      default:
+        return <Typing key={id} message={data} from={from} />;
+    }
   });
 
-  return (
-    <ul>  
-      {messageBlock}
-    </ul>
-  );
+  return <ul>{messageBlock}</ul>;
 }

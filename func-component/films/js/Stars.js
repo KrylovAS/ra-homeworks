41b891b04c221f9655182b6b91fa45feb6cec432
KrylovAS <s1nii@mail.ru>
2018-09-15 15:27:43 +0400

    refactorig

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,13 +1,14 @@
 'use strict';
 
-function Stars({ count }) {
-  if (typeof count !== 'number' && count < 1 && count > 5) return null;
-  const ganerateStar = count => {
+function Stars({ count }) {  
+  const ganerateStar = count => {    
+    if(Number.isInteger(count) && count < 1 || count > 5) return null;    
     const quantityStar = [];
     for (let i = 0; i < count; i++) {
       quantityStar.push(<Star />);
     }
     return quantityStar.map(el => <li>{el}</li>);
   };
+  
   return <ul className="card-body-stars u-clearfix">{ganerateStar(count)}</ul>;
 }

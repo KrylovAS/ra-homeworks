8a75b858d7df01171bdbf70d0adbe16f446b2ff9
KrylovAS <s1nii@mail.ru>
2018-09-17 20:51:20 +0400

    вынес логику вычисления кол-ва в звезд из компонента

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,14 +1,15 @@
 'use strict';
 
-function Stars({ count }) {  
-  const ganerateStar = count => {    
-    if(Number.isInteger(count) && count < 1 || count > 5) return null;    
+const ganerateStar = (count)  => {
+  if(Number.isInteger(count) && count < 1 || count > 5) return null;    
     const quantityStar = [];
     for (let i = 0; i < count; i++) {
       quantityStar.push(<Star />);
-    }
-    return quantityStar.map(el => <li>{el}</li>);
-  };
-  
-  return <ul className="card-body-stars u-clearfix">{ganerateStar(count)}</ul>;
+    }  
+    return quantityStar;
 }
+
+const  Stars = ({ count }) => <ul className="card-body-stars u-clearfix">
+                                {ganerateStar(count).map(el => <li>{el}</li>)}
+                              </ul>;
+
